5cc2f8a27947f104ff85f366cb46b1c6c6b2bebf
drebezovan <drebezovan@mail.ru>
2023-09-16 22:17:51 +0300

    Напиcала программу, которая выводит: “Hello from <ваше_имя>”

---
+++ src/test/java/HelloFromNameTest.java
@@ -0,0 +1,33 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloFromNameTest {
+
+    @Test
+    public void testOutputsHelloFromName(){
+
+        String response = RestAssured
+                .given()
+                .queryParam("name", "Наташа")
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath()
+                .getJsonObject("answer")
+                .toString()
+                .replace(",", " from");
+
+        System.out.println(response);
+    }
+
+    @Test
+    public void testOutputsGetRequestResponse(){
+
+        Response response = RestAssured
+                .given()
+                .queryParam("name", "Наташа")
+                .get("https://playground.learnqa.ru/api/hello?name=Наташа")
+                .andReturn();
+
+        response.prettyPrint();
+    }
+}
