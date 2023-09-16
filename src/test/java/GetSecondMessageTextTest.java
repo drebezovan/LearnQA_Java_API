258c4c89e7dd80942bf7804f892bf45a983ced7e
drebezovan <drebezovan@mail.ru>
2023-09-16 23:32:19 +0300

    Создала тест, который делает GET-запрос и выводит текст второго сообщения

---
+++ src/test/java/GetSecondMessageTextTest.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class GetSecondMessageTextTest {
+
+    @Test
+    public void testGetSecondMessageText(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .andReturn();
+
+        String secondMessageText = response
+                .jsonPath()
+                .getList("messages")
+                .get(1)
+                .toString()
+                .replaceAll("[\\{\\}]", "");
+
+        System.out.println(secondMessageText);
+    }
+}
