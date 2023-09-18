c1319d8e8854f7a01b87ee8182a2e04cc9085859
drebezovan <drebezovan@mail.ru>
2023-09-18 18:08:30 +0300

    Написала тест, который делает следующее: 1) создает задачу 2) делает один запрос с token ДО того, как задача готова, убеждается в правильности поля status 3) ждет нужное количество секунд 4) делает один запрос c token ПОСЛЕ того, как задача готова, убеждается в правильности поля status и наличии поля result

---
+++ src/test/java/GetTaskAndCheckFieldsTest.java
@@ -0,0 +1,49 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class GetTaskAndCheckFieldsTest {
+
+    @Test
+    public void testGetTaskAndCheckFields() throws InterruptedException {
+
+        Response response = getResponseToNewTask();
+
+        String token = response.jsonPath().get("token").toString();
+        int timeToCreateTask = response.jsonPath().get("seconds");
+
+        Response responseWithUnreadyTask = getResponseToTask(token);
+
+        String actualStatusWithUnreadyTask = responseWithUnreadyTask.jsonPath().get("status");
+
+        Assertions.assertEquals("Job is NOT ready", actualStatusWithUnreadyTask);
+
+        TimeUnit.SECONDS.sleep(timeToCreateTask);
+
+        Response responseWithReadyTask = getResponseToTask(token);
+
+        String actualStatusWithReadyTask = responseWithReadyTask.jsonPath().get("status");
+        String actualResultWithReadyTask = responseWithReadyTask.jsonPath().get("result");
+
+        Assertions.assertEquals("Job is ready", actualStatusWithReadyTask);
+        Assertions.assertFalse(actualResultWithReadyTask.isEmpty());
+
+    }
+
+    private static Response getResponseToNewTask() {
+        return RestAssured
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+    }
+
+    private static Response getResponseToTask(String token) {
+        return RestAssured
+                .given()
+                .queryParam("token", token)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+    }
+}
