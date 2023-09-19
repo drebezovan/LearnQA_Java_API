e190dd0c636dee4339ffc99cc4aadce5538ceccb
drebezovan <drebezovan@mail.ru>
2023-09-19 17:47:59 +0300

    С помощью JUnit написала тест, который проверяет длину какой-то переменной типа String с помощью assertThat

---
+++ src/test/java/CheckTheLengthOfVariableTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasLength;
+
+public class CheckTheLengthOfVariableTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "Natasha", "test"})
+    public void checkLengthOfVariable(String parameter){
+
+        assertThat(parameter, hasLength(parameter.length()));
+    }
+
+}
